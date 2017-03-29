ea29103bcf0673c8ff44ed83359d477d152e6e52
Faisal Alkhotaifi <faisalalkhotaifi@Faisals-MacBook-Pro.local>
2017-03-28 18:45:37 -0700

    Initial Commit

---
+++ Swift app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift app
+//
+//  Created by Faisal Alkhotaifi on 3/28/17.
+//  Copyright © 2017 Faisal Alkhotaifi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
