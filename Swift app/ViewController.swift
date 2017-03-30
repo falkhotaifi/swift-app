6c208436fc280052b0eecf1ccc0caf784219ac53
Faisal Alkhotaifi <faisalalkhotaifi@Faisals-MacBook-Pro.local>
2017-03-29 19:10:29 -0700

    Now we can add number

--- Swift app/ViewController.swift
+++ Swift app/ViewController.swift
@@ -11,19 +11,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var n1: UITextField!
+    @IBOutlet weak var n2: UITextField!
+    @IBOutlet weak var result: UITextField!
+    
+   
+    
+    
     var tapCount = 0
     @IBAction func buttonTapped(_ sender: Any) {
-        
+       
+        Label.text = "يا عمري بس"
         tapCount += 1
         print ("TapCount is ", tapCount)
         
         if tapCount >= 10 {
             Label.text = "You just tapped 10 times"
         }
-        /*
-        theLabel.text = "يا عمري بس"
-        print("It works")
-         */
+        
+        
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        result.text = String(Double(n1.text!)! + Double(n2.text!)!)
     }
     
     
